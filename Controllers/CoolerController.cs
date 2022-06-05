7669c556611af38256730b5fa6664bd09182b21c
Nilleren1 <nicolai.hoegsbjerg@gmail.com>
2022-06-05 14:49:17 +0200

    Add project files.

---
+++ Controllers/CoolerController.cs
@@ -0,0 +1,86 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using RestWineCooler.Manager;
+using WineCoolerLib;
+
+namespace RestWineCooler.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CoolerController : ControllerBase
+    {
+        private WineManager _manager = new WineManager();
+
+
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [HttpGet]
+        public ActionResult<List<Cooler>> GetAll()
+        {
+            List<Cooler> result = _manager.GetAllCoolers();
+            if (result.Count() == 0)
+            {
+                NoContent();
+            }
+            return Ok(result);
+        }
+        
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [HttpGet("{id}")]
+        public ActionResult<Cooler> Get(int id)
+        {
+            //Cooler cooler = _manager.GetCooler(id);
+            //if (cooler == null)
+            //{
+            //    NotFound();
+            //}
+            return Ok(_manager.GetCooler(id));
+        }
+        
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [HttpPost]
+        public ActionResult<Cooler> Post([FromBody] Cooler newCooler)
+        {
+            Cooler createdCooler = new Cooler();
+            if (newCooler.Temp > 50)
+            {
+                return BadRequest();
+            }
+            createdCooler = _manager.AddCooler(newCooler);
+            return Created("api/Cooler/" + createdCooler.CoolerId, createdCooler);
+        }
+
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [HttpDelete("{id}")]
+        public ActionResult<Cooler> Delete(int id)
+        {
+            Cooler coolerToBeDeleted = _manager.GetCooler(id);
+
+            _manager.Delete(id);
+            return Ok(coolerToBeDeleted);
+        }
+
+        [ProducesResponseType(StatusCodes.Status409Conflict)]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [HttpGet("{id}/AddWine")]
+        public ActionResult<int> AddWine([FromRoute]int id)
+        {
+            Cooler cooler = _manager.GetCooler(id);
+            if (cooler.CoolerIsFull())
+            {
+                return Conflict();
+            }
+            int result = _manager.AddWine(id);
+            //Den gode dokumentering.
+            return  Ok("Bottles before: "  + (result- 1) + "    Bottles now: "  + result);
+        }
+
+    }
+}
---
+++ Manager/WineManager.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WineCoolerLib;
+
+namespace RestWineCooler.Manager
+{
+    public class WineManager
+    {
+        private static int _nextId = 1;
+        //Cooler tilføjet via dll fil reference.
+        private static List<Cooler> coolersList = new List<Cooler>()
+        {
+            new Cooler() {CoolerId = _nextId++, Temp = 15, CapacityOfBottles = 20, BottlesInStorage = 11},
+            new Cooler() {CoolerId = _nextId++, Temp = 12, CapacityOfBottles = 10, BottlesInStorage = 2},
+            new Cooler() {CoolerId = _nextId++, Temp = 13, CapacityOfBottles = 30, BottlesInStorage = 13},
+            new Cooler() {CoolerId = _nextId++, Temp = 14, CapacityOfBottles = 20, BottlesInStorage = 5},
+        };
+
+        public List<Cooler> GetAllCoolers()
+        {
+            return coolersList;
+        }
+
+        public Cooler GetCooler(int id)
+        {
+            return coolersList.Find(c => c.CoolerId == id);
+        }
+
+        public Cooler AddCooler(Cooler cooler)
+        {
+            cooler.CoolerId = _nextId++;
+            coolersList.Add(cooler);
+            return cooler;
+        }
+
+        public Cooler Delete(int id)
+        {
+            Cooler coolerToBeDeleted = GetCooler(id);
+            coolersList.Remove(coolerToBeDeleted);
+            return coolerToBeDeleted;
+        }
+
+        public int AddWine(int coolerId)
+        {
+            Cooler cooler = GetCooler(coolerId);
+
+            int result = cooler.AddWine();
+
+            return result;
+        }
+    }
+}
